7d8b67e6dcefed32d1b36d3df3ab19ce237d5579
Chris Cantrell <topherCantrell@gmail.com>
2022-01-08 12:24:41 -0600

    good enough

--- terminal/server.py
+++ terminal/server.py
@@ -2,6 +2,9 @@ import tornado.ioloop
 import tornado.web
 import tornado.websocket
 
+import serial
+import threading
+
 '''
 sudo python3 -m pip install tornado
 
@@ -17,29 +20,79 @@ cd /home/pi/terminal
 python3 server.py  
 '''
 
-class CaseHandler(tornado.web.RequestHandler):
+class UploadHandler(tornado.web.RequestHandler):
+
+    # address, binary, [optional execute address]
 
     def get(self):
         text = self.get_argument('text', '')
         self.write(text.upper())
 
 class WebsocketHandler(tornado.websocket.WebSocketHandler):
+
+    def open(self):
+        global web_clients
+        web_clients.add(self)
+
+    def on_close(self):
+        global web_clients
+        if self in web_clients:
+            web_clients.remove(self)
         
     def on_message(self, message):
         """
         Message received on channel
         """
-        print("Got a message",message)
-        self.write_message(message)
+        if message=='Enter':
+            message = '\x0D'        
+        ser.write(message.encode())
+
+def broadcast(message):
+    global web_clients
+    for client in web_clients:
+        try:
+            client.write_message(message)
+        except Exception:
+            pass
+
+ser = serial.Serial('/dev/ttyUSB0',baudrate=115200)
+
+def watchSBC():    
+    while True:
+        c = ser.read(1)       
+        # print('>',c,'<') 
+        if c==b'\x0A':
+            message = '\x0A'       
+        elif c==b'\x0D':
+            message = ''          
+        else:
+            try:
+                message = c.decode()        
+            except Exception:
+                message = ''
+        if message:
+            io_loop.add_callback(broadcast,message)            
 
 handlers = [
-    (r"/case",CaseHandler),
+    (r"/upload",UploadHandler),
     (r"/web-socket", WebsocketHandler,{}),
     (r"/(.*)", tornado.web.StaticFileHandler, {        
         "path": "/home/pi/terminal/webroot", 
         "default_filename": "index.html"}),
 ]
 
+# TODO: Convert backspace and enter to "Backspace" and "Enter". Ignore all other non-char keys.
+# TODO: POST for uploading code
+
+# TODO: watch serial port for incoming chars (from board) and pass
+#       them on to the websocket
+# TODO: pass keys from the websocket to the serial
+
+web_clients = set()
+
+threading.Thread(target=watchSBC).start()
+
 app = tornado.web.Application(handlers)
 app.listen(80)
-tornado.ioloop.IOLoop.current().start()
+io_loop = tornado.ioloop.IOLoop.current()
+io_loop.start()
--- terminal/webroot/terminal.js
+++ terminal/webroot/terminal.js
@@ -1,18 +1,90 @@
 
-function onStart() {
-    ws = new WebSocket('ws://'+document.location.host+'/web-socket')
-    //ws = new WebSocket('ws://10.0.0.15/web-socket')
-    ws.onopen = function() {
-        ws.send('READY\n')
+var ws // The websocket to the server
+var vp = document.getElementById("viewText") // The textarea
+var text = ""
+vp.value = text
+
+function onOpen() {
+    ws.send('Enter')    
+}
+
+function showKey(evt) {
+    // Events coming up from the server
+    for(var i=0;i<evt.data.length;++i) {
+        c = evt.data[i]
+        co = c.charCodeAt(0)    
+        text = text.replace('\u2588','')
+        
+        if(co==0x0A) {
+            numspaces = 80 - (text.length % 80)
+            for(var i=0;i<numspaces;i++) {
+                text += ' '
+            }
+        } else if(co==0x08) {
+            text = text.slice(0,-1)
+        } else if(co<32 || co>126) {
+            text += '~'
+        } 
+        else {        
+            text += c
+           
+        }
+
+        text += '\u2588'
+
+        while(text.length>31*80) {
+            text = text.slice(80)
+        }
+
     }
-    ws.onmessage = function(evt) {
-        console.log(evt.data)  
-        document.getElementById("viewText").value += evt.data;
-    }
+     
+    vp.value = text    
+}
+
+function showKeyOLD(evt) {
+    // Events coming up from the server
+    c = evt.data
+    co = c.charCodeAt(0)    
+    //text = text.replace('\u2588','')
+    if(co==0x0A) {          
+        var numspaces = 80 - text.length % 80      
+        for(var i=0;i<numspaces;++i) {
+            text += ' '
+        }
+    } else if(co==0x08) {
+        text = text.slice(0,-1)
+    } else if(co<32 || co>126) {
+        text += '`'
+    } else {
+        text+=c
+    } 
+    //text += '\u2588'
+    vp.innerText = text
+    evt.stopPropagation();
+    return false;
+
+    // TODO: cursor character, limit chars so no scroll bar, handle backspace
 }
 
 function handleKey(evt) {
-    ws.send(evt.key)
+    // Keyboard events to be send to the server
+    if(evt.key=='Backspace') {
+        ws.send(String.fromCharCode(0x08))
+    } else if(evt.key=='Enter') {
+        ws.send(String.fromCharCode(0x0D))   
+    } else if(evt.key.length==1) {
+        if(!evt.altKey && !evt.ctrlKey) {
+            ws.send(evt.key)
+        }
+    } else {
+        // Ignore other special keys
+    }
+}
+
+function onStart() {
+    ws = new WebSocket('ws://'+document.location.host+'/web-socket')
+    ws.onopen = onOpen
+    ws.onmessage = showKey
 }
 
 onStart()
